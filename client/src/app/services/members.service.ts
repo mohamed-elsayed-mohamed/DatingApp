00c660aa147e420c97ffa05b9f613145d92cee9a
Mohamed Elsayed <mohamed_elsayed.1995@yahoo.com>
2023-09-27 05:10:29 +0300

    Update User Profile

--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -2,20 +2,40 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member';
+import { map, of, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) {}
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members.length > 0) return of(this.members);
+
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map((members) => {
+        this.members = members;
+        return this.members;
+      })
+    );
   }
 
   getMember(username: string) {
+    const member = this.members.find((m) => m.userName === username);
+    if (member) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const indx = this.members.indexOf(member);
+        this.members[indx] = { ...this.members[indx], ...member };
+      })
+    );
   }
 }
