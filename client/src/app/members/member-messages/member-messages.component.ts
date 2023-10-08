2bd5be24056fdad65af74d31e658ba44c5506bcd
Mohamed Elsayed <mohamed_elsayed.1995@yahoo.com>
2023-10-09 01:12:13 +0300

    Add messages

---
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -0,0 +1,36 @@
+import { TimeagoModule } from 'ngx-timeago';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Message } from 'src/app/models/Message';
+import { MessageService } from 'src/app/services/message.service';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-member-messages',
+  standalone: true,
+  templateUrl: './member-messages.component.html',
+  styleUrls: ['./member-messages.component.css'],
+  imports: [CommonModule, TimeagoModule, FormsModule],
+})
+export class MemberMessagesComponent implements OnInit {
+  @ViewChild('messageForm') messageForm?: NgForm;
+  @Input() username?: string;
+  @Input() messages: Message[] = [];
+  messageContent = '';
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {}
+
+  sendMessage() {
+    if (!this.username) return;
+    this.messageService
+      .sendMessage(this.username, this.messageContent)
+      .subscribe({
+        next: (message) => {
+          this.messages.push(message);
+          this.messageForm?.reset();
+        },
+      });
+  }
+}
